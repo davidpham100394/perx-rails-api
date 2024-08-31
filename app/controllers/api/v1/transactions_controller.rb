88de23f26b27705224a2fd5217d7667f2073bb10
Luong Pham (David) <148191390+luongpq1003@users.noreply.github.com>
2024-08-31 18:30:31 +0700

    Add rubocop config
    Create Rails CICD
    
    Setup ruby version in the CI/CD file
    
    Update CI/CD file
    
    Update ruby version
    
    Update CI file
    
    Remove linter step
    
    Update postgres password
    
    Add rspec gem
    
    Update rspec
    
    Add annotate gem

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -1,40 +1,39 @@
+# frozen_string_literal: true
+
 module Api
-    module V1
-        class TransactionsController < ApplicationController
-            skip_before_action :verify_authenticity_token
-            def single
-                transaction = Transaction.new(transaction_params)
+  module V1
+    class TransactionsController < ApplicationController
+      skip_before_action :verify_authenticity_token
+      def single
+        transaction = Transaction.new(transaction_params)
 
-                if transaction.save
-                render json: { status: 'success', transaction_id: transaction.transaction_id }, status: :created
-                else
-                render json: { status: 'error', errors: transaction.errors.full_messages }, status: :unprocessable_entity
-                end
-            end
+        if transaction.save
+          render json: { status: 'success', transaction_id: transaction.transaction_id }, status: :created
+        else
+          render json: { status: 'error', errors: transaction.errors.full_messages },
+                 status: :unprocessable_entity
+        end
+      end
 
-            private
+      def bulk
+        transactions = Transaction.create(bulk_transaction_params[:transactions])
+        if transactions.all?(&:persisted?)
+          render json: { status: 'success', processed_count: transactions.size }, status: :created
+        else
+          render json: { status: 'error', errors: transactions.map(&:errors).map(&:full_messages) },
+                 status: :unprocessable_entity
+        end
+      end
 
-            def transaction_params
-                params.require(:transaction).permit(:transaction_id, :points, :user_id)
-            end
+      private
 
-            def bulk
-                transactions = Transaction.create(bulk_transaction_params[:transactions])
+      def transaction_params
+        params.require(:transaction).permit(:transaction_id, :points, :user_id)
+      end
 
-                if transactions.all?(&:persisted?)
-                    render json: { status: 'success', processed_count: transactions.size }, status: :created
-                else
-                    render json: { status: 'error', errors: transactions.map(&:errors).map(&:full_messages) }, status: :unprocessable_entity
-                end
-            end
-
-            private
-
-            def bulk_transaction_params
-                arams.permit(transactions: [:transaction_id, :points, :user_id])
-            end
-
-            end
+      def bulk_transaction_params
+        params.permit(transactions: %i[transaction_id points user_id])
+      end
     end
+  end
 end
-  
